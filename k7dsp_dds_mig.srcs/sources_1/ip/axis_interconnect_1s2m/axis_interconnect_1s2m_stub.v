61eca038d4fe7ee419faa8a4da02b85b7e85125c
samprager <prager.sam@gmail.com>
2016-06-03 22:02:09 -0700

    successfull transmission of data from adc over ethernet

--- k7dsp_dds_mig.srcs/sources_1/ip/axis_interconnect_1s2m/axis_interconnect_1s2m_stub.v
+++ k7dsp_dds_mig.srcs/sources_1/ip/axis_interconnect_1s2m/axis_interconnect_1s2m_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2014.2 (win64) Build 932637 Wed Jun 11 13:33:10 MDT 2014
-// Date        : Thu Jun 02 18:25:34 2016
+// Date        : Fri Jun 03 21:36:41 2016
 // Host        : radar-PC running 64-bit Service Pack 1  (build 7601)
 // Command     : write_verilog -force -mode synth_stub
 //               D:/UndergroundRadar/kc705_dds_mig/k7dsp_dds_mig.srcs/sources_1/ip/axis_interconnect_1s2m/axis_interconnect_1s2m_stub.v
